030032d18973837eba5ca5e705a204a6ea9f8ec0
mnovok <mnovok00@fesb.hr>
2022-12-19 19:29:12 +0100

    added buttons

--- routes/auth.js
+++ routes/auth.js
@@ -3,7 +3,7 @@ const authController = require('../controllers/auth');
 
 const router = express.Router();
 
-router.post('/register', authController.register)
+router.post('/signup', authController.register)
 
 module.exports = router;
 
--- routes/pages.js
+++ routes/pages.js
@@ -3,15 +3,15 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('index');
+    res.render('homepage');
 });
 
 router.get('/register', (req, res) => {
-    res.sendFile("signup.html", {root:"./public"} );
+    res.render("signup.html", {root:"./public"} );
 });
 
 router.get('/login', (req, res) => {
-    res.sendFile("login.html", {root:"./public"} );
+    res.render("login.html", {root:"./public"} );
 });
 
 module.exports = router;
